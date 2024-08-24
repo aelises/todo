4057ff58748c87de3b4e82a54c4c642deea7625a
aelises <aelises@yahoo.com>
2024-08-24 15:56:06 +0800

    Create a static web interface, add new items feature

--- web.py
+++ web.py
@@ -3,6 +3,13 @@ import functions
 
 todos = functions.get_todos()
 
+
+def add_todo():
+    todo = st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions.write_todos(todos)
+
+
 st.title("My todo App")
 st.subheader("This is my todo app.")
 st.write("This is to increase your productivity.")
@@ -13,5 +20,7 @@ st.checkbox("Throw trash")
 for todo in todos:
     st.checkbox(todo)
 
-st.text_input(label="Todo label", placeholder="Add new todo...")
+st.text_input(label="Todo label", placeholder="Add new todo...",
+              on_change=add_todo, key="new_todo")
 
+st.session_state
